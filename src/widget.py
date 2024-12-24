28599f1c5409dd98775e89a7b3d40d351f8babe5
Maksim Charin <bonfiglioly@gmail.com>
2024-12-24 15:17:00 +0300

    update docstring

--- src/widget.py
+++ src/widget.py
@@ -2,7 +2,9 @@ from masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(input_values: str) -> str:
-    """Функция принимает строку, содержащую тип и номер карты или счета."""
+    """Функция принимает строку, содержащую тип и номер карты или счета.
+    Возвращает зашифрованное значение"""
+
     input_values_list = input_values.split()
     input_values_name = input_values_list[0]
 
@@ -33,18 +35,18 @@ def get_date(date_str: str) -> str:
     Функция принимает строку с датой в формате "2024-03-11T02:26:18.671407"
     и возвращает строку с датой в формате "ДД.ММ.ГГГГ".
     """
-    date_list = date_str.split('T')
-    date_part = date_list[0].split('-')
+    date_list = date_str.split("T")
+    date_part = date_list[0].split("-")
 
     dd = date_part[2]
     mm = date_part[1]
     yy = date_part[0]
 
-    return f'{dd}.{mm}.{yy}'
+    return f"{dd}.{mm}.{yy}"
 
 
 if __name__ == "__main__":
     print(mask_account_card("Счет 73654108430135874305"))
     print(mask_account_card("Maestro 1596837868705199"))
     print(mask_account_card("MasterCard 7158300734726758"))
-    print(get_date('2024-03-11T02:26:18.671407'))
+    print(get_date("2024-03-11T02:26:18.671407"))
