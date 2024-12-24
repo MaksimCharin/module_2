aabbc929f66a334e49ff45811f32ac76b3f3dc0b
Maksim Charin <bonfiglioly@gmail.com>
2024-12-24 15:14:02 +0300

    add new func (get_date)

--- src/widget.py
+++ src/widget.py
@@ -28,10 +28,23 @@ def mask_account_card(input_values: str) -> str:
         return "Вы ввели некорректное название карты/номера счета"
 
 
+def get_date(date_str: str) -> str:
+    """
+    Функция принимает строку с датой в формате "2024-03-11T02:26:18.671407"
+    и возвращает строку с датой в формате "ДД.ММ.ГГГГ".
+    """
+    date_list = date_str.split('T')
+    date_part = date_list[0].split('-')
+
+    dd = date_part[2]
+    mm = date_part[1]
+    yy = date_part[0]
+
+    return f'{dd}.{mm}.{yy}'
+
+
 if __name__ == "__main__":
     print(mask_account_card("Счет 73654108430135874305"))
     print(mask_account_card("Maestro 1596837868705199"))
     print(mask_account_card("MasterCard 7158300734726758"))
-    print(mask_account_card("Visa Classic 6831982476737658"))
-    print(mask_account_card("Visa Platinum 8990922113665229"))
-    print(mask_account_card("Visa Gold 5999414228426353"))
+    print(get_date('2024-03-11T02:26:18.671407'))
